dd65416e89cea7a941f82f5066c58ced912c0c43
mikhailrezchikov <mrezchikov529@gmail.com>
2023-06-29 22:55:51 +0300

    tests added

---
+++ src/test/java/qa/guru/allure/WebSteps.java
@@ -0,0 +1,39 @@
+package qa.guru.allure;
+
+import io.qameta.allure.Step;
+
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static org.openqa.selenium.By.linkText;
+
+public class WebSteps {
+
+    @Step("Открываем главную страницу")
+    public void openMainPage() {
+        open("https://github.com");
+    }
+
+    @Step("Ищем репозиторий {repo}")
+    public void searchForRepository(String repo) {
+        $(".header-search-input").click();
+        $(".header-search-input").sendKeys(repo);
+        $(".header-search-input").submit();
+    }
+
+    @Step("Кликаем по ссылке репозитория {repo}")
+    public void clickOnRepositoryLink(String repo) {
+        $(linkText(repo)).click();
+    }
+
+    @Step("Открываем таб Issues")
+    public void openIssuesTab() {
+        $("#issues-tab").click();
+    }
+
+    @Step("Проверяем наличие Issue с именем {issueName}")
+    public void shouldSeeIssueWithName(String issueName) {
+        $(byText(issueName)).shouldHave(visible);
+    }
+}
